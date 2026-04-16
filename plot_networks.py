3b029d8f9995964d850d7322c9ce36f48bb48451
tim-lemm <tim3lem@gmail.com>
2026-04-16 17:40:57 +0200

    sioux falls data preprocessing (greenspaces data colection)

--- plot_networks.py
+++ plot_networks.py
@@ -3,7 +3,10 @@ from utils_od_matrix_generator import generate_od_df
 from utils_plotting import *
 from utils_network_processing import *
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 30})
+# plt.rcParams.update({'font.size': 30})
+
+
+
 
 # list_test_name = ["grid","H","tunnel","tunnel_ng"]
 # for test_name in list_test_name:
@@ -26,37 +29,38 @@ plt.rcParams.update({'font.size': 30})
 #
 #     plt.savefig(f"output/_hEART_article/figures/_networks/{test_name}_networks.png")
 
-list_test_name = ["grid","H","tunnel"]
-fig, ax = plt.subplots(3,1, figsize=(15,30))
-for i, test_name in enumerate(list_test_name):
-    edge_df, node_df = import_network(f"data/edges_{test_name}.csv", f"data/nodes_{test_name}.csv")
-    plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title=test_name, ax=ax[i])
-ax[0].set_title("Grid Network\n (scenario A)")
-ax[1].set_title("H Network\n (scenario B)")
-ax[2].set_title("Tunnel Network\n (scenario C and D)")
-plt.tight_layout()
-plt.savefig(f"output/_hEART_article/figures/_networks/_all_simple_network.png")
+# list_test_name = ["grid","H","tunnel"]
+# fig, ax = plt.subplots(3,1, figsize=(15,30))
+# for i, test_name in enumerate(list_test_name):
+#     edge_df, node_df = import_network(f"data/edges_{test_name}.csv", f"data/nodes_{test_name}.csv")
+#     plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title=test_name, ax=ax[i])
+# ax[0].set_title("Grid Network\n (scenario A)")
+# ax[1].set_title("H Network\n (scenario B)")
+# ax[2].set_title("Tunnel Network\n (scenario C and D)")
+# plt.tight_layout()
+# plt.savefig(f"output/_hEART_article/figures/_networks/_all_simple_network.png")
+# plt.show()
 
-fig, ax = plt.subplots(figsize=(15,15))
-size_od = max(node_df['id']) + 1
-od_df = generate_od_df(size_od, od_scenario="CORNER_2", max_demand=4000)
-plot_od_matrix(convert_to_eaquilibrae_od_matrix(od_df), edge_df, node_df, ax=ax)
-plt.savefig("output/figures/od_matrix.png")
-
+# fig, ax = plt.subplots(figsize=(15,15))
+# size_od = max(node_df['id']) + 1
+# od_df = generate_od_df(size_od, od_scenario="CORNER_2", max_demand=4000)
+# plot_od_matrix(convert_to_eaquilibrae_od_matrix(od_df), edge_df, node_df, ax=ax)
+# plt.savefig("output/figures/od_matrix.png")
+# plt.show()
 # edge_df, node_df = import_network(f"data/edges_tunnel.csv", f"data/nodes_tunnel.csv")
 # plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title=f"Test network (tunnel) with topology and green spaces", figsize= (10,10))
 # plt.savefig(f"output/_hEART_article/figures/_networks/empty_simple_network_tunnel.png", transparent=True)
 
-fig, ax = plt.subplots(2,1, figsize=(15,30))
-edge_df, node_df = import_network("data/edges_grid.csv", "data/nodes_grid.csv")
-plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Green overlap percentage", ax=ax[0], color_col_num="green_overlap_percentage", cmap="Greens")
-plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Slope (%)", ax=ax[1], color_col_num="slope", cmap="bwr")
-plt.tight_layout()
-plt.savefig("output/_hEART_article/figures/_networks/grid_networks.png")
+# fig, ax = plt.subplots(2,1, figsize=(15,30))
+# edge_df, node_df = import_network("data/edges_grid.csv", "data/nodes_grid.csv")
+# plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Green overlap percentage", ax=ax[0], color_col_num="green_overlap_percentage", cmap="Greens")
+# plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Slope (%)", ax=ax[1], color_col_num="slope", cmap="bwr")
+# plt.tight_layout()
+# plt.savefig("output/_hEART_article/figures/_networks/grid_networks.png")
 
-fig, ax = plt.subplots(2,1, figsize=(15,30))
-edge_df, node_df = import_network("data/edges_tunnel.csv", "data/nodes_tunnel.csv")
-plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Green overlap percentage", ax=ax[0], color_col_num="green_overlap_percentage", cmap="Greens")
-plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Slope (%)", ax=ax[1], color_col_num="slope", cmap="bwr")
-plt.tight_layout()
-plt.savefig("output/_hEART_article/figures/_networks/tunnel_networks.png")
+# fig, ax = plt.subplots(2,1, figsize=(15,30))
+# edge_df, node_df = import_network("data/edges_tunnel.csv", "data/nodes_tunnel.csv")
+# plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Green overlap percentage", ax=ax[0], color_col_num="green_overlap_percentage", cmap="Greens")
+# plot_network(edge_df, node_df, node_id_col='id',base_width=1, node_label=True, title="Slope (%)", ax=ax[1], color_col_num="slope", cmap="bwr")
+# plt.tight_layout()
+# plt.savefig("output/_hEART_article/figures/_networks/tunnel_networks.png")
--- test_Sioux_Falls_Data.py
+++ test_Sioux_Falls_Data.py
@@ -1,7 +1,25 @@
 from utils_plotting import *
 from utils_network_processing import *
 import matplotlib.pyplot as plt
+import osmnx as ox
+import geopandas as gpd
+from shapely.geometry import Point
 
+
+fig,ax = plt.subplots()
 edge_df, node_df = import_network("data/Sioux_Falls/edges_Sioux_Falls.csv", "data/Sioux_Falls/nodes_Sioux_Falls.csv")
-plot_network(edge_df, node_df, node_label=True, title="Sioux Falls Network")
+plot_network(edge_df, node_df, node_label=True, title="Sioux Falls Network", ax=ax)
+
+tags = { 'leisure': ['park','dog_park','pitch','garden','nature_reserve','marina'],
+               'landuse':['forest','meadow','grassland','farmland','farmyard','vineyard','orchard','farmland','recreation_ground','allotments','village_green','grass','greenfield','bassin'],
+               'grassland': True,
+               'natural': ['water','bay'],
+                'surface': 'grass',
+                'wetland' : True }
+gdf_park_SF = ox.features_from_place("Sioux Falls",tags)
+gdf_park_SF = gdf_park_SF[gdf_park_SF.geom_type.isin(['Polygon', 'MultiPolygon'])]
+gdf_park_SF = gdf_park_SF.to_crs(epsg=4326)
+gdf_park_SF.plot(ax=ax, color="green")
 plt.show()
+
+gdf_park_SF.to_csv("data/Sioux_Falls/green_Sioux_Falls.csv")

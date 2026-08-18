7ddbac94287de970888789e55a1cbecf9e3487fa
tim-lemm <tim3lem@gmail.com>
2026-08-18 11:23:04 +0200

    plot of comparaison with BIMODEL on SF

--- test.py
+++ test.py
@@ -1,15 +1,35 @@
-from utils_model_handeling import *
-from utils_traffic import *
-from utils_network_processing import *
-from utils_plotting import *
-from utils_od_matrix_generator import generate_od_df
-from config import parameter
-from utils_traffic import *
-from utils_plotting import *
-from utils_optimization import *
+import matplotlib.pyplot as plt
+import pandas as pd
+plt.rcParams.update({'font.size': 30})
 
-edge_df, node_df = import_network("data/Sioux_Falls/edges_Sioux_Falls.csv", "data/Sioux_Falls/nodes_Sioux_Falls.csv", real_network=True)
+city_name = "Sioux_Falls"
+name_test = f"CAP_{city_name}_test_from_scratch_2"
+fig, ax = plt.subplots(3, 1, figsize=(30, 30))
+fp_nobimodel = f"output/optimization/rgo_results_df_opt_{name_test}.csv"
+fp_bimodel = f"output/optimization/rgo_results_df_opt_CAP_Sioux_Falls_BIMODEL_test_from_scratch_2.csv"
+df_nobimodel = pd.read_csv(fp_nobimodel)
+df_bimodel = pd.read_csv(fp_bimodel)
 
-edge_df = calculate_length_bi_3(edge_df, "GradientBoostingClassifier")
+# Plot primaire
+ax[0].plot(df_bimodel["nbr_bike_lanes"], df_bimodel["modal_share_bike"], linewidth=2, label = "Bi model")
+ax[0].plot(df_nobimodel["nbr_bike_lanes"], df_nobimodel["modal_share_bike"], linewidth=2, label = "No Bi model")
+ax[1].plot(df_bimodel["nbr_bike_lanes"], df_bimodel["flow_of_removed_edge"].iloc[::-1].values, linewidth=2, label = "Bi model")
+ax[1].plot(df_nobimodel["nbr_bike_lanes"], df_nobimodel["flow_of_removed_edge"].iloc[::-1].values, linewidth=2, label = "No Bi model")
+ax[2].plot(df_bimodel["nbr_bike_lanes"], df_bimodel["average_bi_coef"].values, linewidth=2, label = "Bi model")
+ax[2].plot(df_nobimodel["nbr_bike_lanes"], df_nobimodel["average_bi_coef"].values, linewidth=2, label = "No Bi model")
 
-print(edge_df.to_string())
+ax[2].set_xlabel("Number of dedicated bike lanes")
+ax[0].set_ylabel("Bicycle modal share (%)")
+ax[1].set_ylabel("Flow of least used edge")
+ax[2].set_ylabel("Average bikeability coefficient")
+ax[0].grid(True, alpha=0.3)
+ax[1].grid(True, alpha=0.3)
+ax[2].grid(True, alpha=0.3)
+ax[0].legend(loc="lower right")
+ax[1].legend(loc="lower right")
+ax[2].legend(loc="lower right")
+
+fig.suptitle("Comparaison of results on Sioux Falls Network, with and without Bikeability Model.")
+
+plt.tight_layout()
+plt.savefig(f"output/test_comparaison/comparaison_SF_BIMODEL_NOBIMODEL.png")
